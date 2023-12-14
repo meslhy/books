9387cd31e957a58acd0c8a839ae85aec51b8aa6a
meslhy <100445840+meslhy@users.noreply.github.com>
2023-12-14 10:39:19 +0300

    finished ui of home screen...

---
+++ lib/domain/di/di.config.dart
@@ -0,0 +1,36 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// InjectableConfigGenerator
+// **************************************************************************
+
+// ignore_for_file: type=lint
+// coverage:ignore-file
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:connectivity_plus/connectivity_plus.dart' as _i3;
+import 'package:get_it/get_it.dart' as _i1;
+import 'package:injectable/injectable.dart' as _i2;
+
+import '../../ui/screens/splash/splash_view_model.dart' as _i4;
+import 'app_module.dart' as _i5;
+
+extension GetItInjectableX on _i1.GetIt {
+// initializes the registration of main-scope dependencies inside of GetIt
+  _i1.GetIt init({
+    String? environment,
+    _i2.EnvironmentFilter? environmentFilter,
+  }) {
+    final gh = _i2.GetItHelper(
+      this,
+      environment,
+      environmentFilter,
+    );
+    final appModule = _$AppModule();
+    gh.factory<_i3.Connectivity>(() => appModule.getConnectivity());
+    gh.factory<_i4.SplashViewModel>(() => _i4.SplashViewModel());
+    return this;
+  }
+}
+
+class _$AppModule extends _i5.AppModule {}
